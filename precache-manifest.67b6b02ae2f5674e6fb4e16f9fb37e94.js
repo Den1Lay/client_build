97274da2143b9a634e8ef3eb68e1359ae29cb2bb
Des_Flex <denilay13902@gmail.com>
2020-09-07 01:34:54 +0500

    Restart

---
+++ precache-manifest.67b6b02ae2f5674e6fb4e16f9fb37e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fcac445aada4cdaa416146e089e22af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5e669ced460cd2abd662",
+    "url": "./static/css/main.b1b41a09.chunk.css"
+  },
+  {
+    "revision": "4fdb9d0401bb8c002acb",
+    "url": "./static/js/2.6d13d4cd.chunk.js"
+  },
+  {
+    "revision": "ce128fb6cc374532e014050637390553",
+    "url": "./static/js/2.6d13d4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e669ced460cd2abd662",
+    "url": "./static/js/main.582ccd28.chunk.js"
+  },
+  {
+    "revision": "d90c5b93796701d5570a",
+    "url": "./static/js/runtime-main.8b0ae790.js"
+  }
+]);
